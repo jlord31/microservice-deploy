22ad869ef851a1dd4e9483da432ff8b748d22c0b
benjamin Oguntoye <benjy@DESKTOP-0I9LGAT.localdomain>
2022-09-02 14:29:53 +0300

    v1

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a541480d04eb54ef996b8d14e99cb524-2098055360.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
